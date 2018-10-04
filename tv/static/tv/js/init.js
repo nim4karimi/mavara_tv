d998e03989235a2be56be1ba9a9ba0cc9e7b408b
Nima Karimi <nima.karimi@live.com>
2018-10-04 12:09:13 +0330

    Add - Navbar

---
+++ tv/static/tv/js/init.js
@@ -0,0 +1,9 @@
+M.AutoInit();
+
+
+
+// Js init for function of navbar
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
