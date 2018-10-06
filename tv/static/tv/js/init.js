29f1b9f746fd48fafcfb2ec76c7154d4de0e6bbd
Nima Karimi <nima.karimi@live.com>
2018-10-06 20:37:27 +0330

    Add - Navbar + icone

--- tv/static/tv/js/init.js
+++ tv/static/tv/js/init.js
@@ -22,3 +22,11 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.fixed-action-btn');
     var instances = M.FloatingActionButton.init(elems);
   });
+
+
+// Js for caroasel
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+  });
