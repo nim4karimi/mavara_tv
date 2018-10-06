dd6afe469ff9bedb97078e04d9c9ee63bbfa48af
Nima Karimi <nima.karimi@live.com>
2018-10-06 20:18:23 +0330

    Add - Navbar + icone

--- tv/static/tv/js/init.js
+++ tv/static/tv/js/init.js
@@ -5,5 +5,20 @@ M.AutoInit();
 // Js init for function of navbar
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+    var instances = M.Sidenav.init(elems);
+  });
+
+
+// Js for parallex
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+  });
+
+// Js for floating btn
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems);
   });
